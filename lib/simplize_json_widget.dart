5e297c30356edd8bd88bb6b2a80b208e2d077d76
Son Tung <tungtshp99@gmail.com>
2025-06-11 09:34:28 +0700

    move simplize json widget

--- lib/simplize_json_widget.dart
+++ lib/simplize_json_widget.dart
@@ -1,5 +1,4 @@
 import 'package:dynamic_widget/dynamic_widget.dart';
-import 'package:dynamic_widget/dynamic_widget/widget/root_widget.dart';
 import 'package:dynamic_widget/dynamic_widget/widget/widget.dart';
 import 'package:flutter/material.dart';
 
